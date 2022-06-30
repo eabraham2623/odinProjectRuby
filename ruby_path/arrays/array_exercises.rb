b68c834abcdd62b53a15a54c8a9a1083fabc1de2
Emil A <eabraham2623@gmail.com>
2022-06-30 19:06:18 -0400

    Added additonal ruby exercise

--- ruby_path/arrays/array_exercises.rb
+++ ruby_path/arrays/array_exercises.rb
@@ -4,3 +4,9 @@ end
 
 puts checkNumberExists([3,53], 3)
 puts checkNumberExists([3,53], 5)
+
+arr = [["test", "hello", "world"],["example", "mem"]]
+arr2 = [15, 7, 18, 5, 12, 8, 5, 1]
+
+puts arr2.index(5)
+puts arr2[5]
