26af2a46f40f25b55b29cf8018185ce5b2e159a1
Emil A <eabraham2623@gmail.com>
2022-09-05 09:57:21 -0400

    Adding max sub array ruby script, sliding window

--- ruby_path/stock_picker/maximum_subarray.rb
+++ ruby_path/stock_picker/maximum_subarray.rb
@@ -1,14 +1,30 @@
-# Return the maximum sum, given the sub array size of k
-def maximumSubarray(arr, subArraySize)
-    maximum = 0
-    initialIndex = 0
+def subArraySum(arr, initialSize)
+    subArray = arr.slice(0, initialSize)
     sum = 0
 
-    while initialIndex < subArraySize
-        sum = sum + arr[initialIndex] 
-        initialIndex = initialIndex + 1
+    subArray.each do |element|
+        sum = sum + element
     end
-    runningIndex = 1
+    return sum
 end
 
-maximumSubarray([3,1,8,4], 3)
+# Return the maximum sum, given the sub array size of k
+def maximumSubarray(arr, subArraySize)
+    # Set maximum sum as initial summation
+    # initial summation to be used to calculate running sums in main loop
+    maximumSum = subArraySum(arr, subArraySize)
+    currentSubArraySum = maximumSum
+
+    # Starting at 2nd index, loop through until reaching index that is the last subarray
+    for index in 1..arr.size()-subArraySize
+        # Subtract the previous index's value from current subarray and add the next index's value
+        currentSubArraySum = (currentSubArraySum - arr[index - 1]) + arr[index + subArraySize - 1]
+        if currentSubArraySum > maximumSum 
+            maximumSum = currentSubArraySum
+        end
+    end
+    return maximumSum
+end
+
+
+puts(maximumSubarray([1,2,3,4,5,6],3))
