9c964051b591b6515ed135d455e5a3fcd20a303d
eabraham2623 <eabraham2623@gmail.com>
2022-10-25 16:45:20 -0400

    Adding entry hash attribute

--- ruby_path/ruby_oo/tic_tac_toe/entry.rb
+++ ruby_path/ruby_oo/tic_tac_toe/entry.rb
@@ -2,11 +2,12 @@
 
 # This class defines the entry object in a Tic Tac Toe Game
 class Entry
-  attr_accessor :position, :type
+  attr_accessor :position, :type, :tic_tac_toe_hash
 
   def initialize(entry_type, position)
     self.type = entry_type
     self.position = position
+    self.tic_tac_toe_hash = { position: self.position, type: type }
   end
 
   def to_s
@@ -16,3 +17,4 @@ end
 
 entry1 = Entry.new('X', 1)
 puts(entry1.to_s)
+puts(entry1.tic_tac_toe_hash)
