d42a4e2504e9a98cc1f66c77fd7f48659ef507a5
eabraham2623 <eabraham2623@gmail.com>
2022-10-25 18:48:33 -0400

    rubocop linting

--- ruby_path/ruby_oo/tic_tac_toe/Board.rb
+++ ruby_path/ruby_oo/tic_tac_toe/Board.rb
@@ -28,11 +28,44 @@ class Board
     end
   end
 
+  def print_entries(entries, column)
+    true_column = column + 1
+    if entries.key?(true_column)
+      if true_column % 3 == 1
+        print("#{entries[true_column]} |")
+      elsif true_column % 3 == 2
+        print(" #{entries[true_column]} |")
+      else
+        print(" #{entries[true_column]}\n")
+      end
+    elsif true_column % 3 == 1
+      print('- |')
+    elsif true_column % 3 == 2
+      print(' - |')
+    else
+      print(' -')
+    end
+  end
+
   def print_empty_board
     board_row_range = (0..4)
     board_row_range.each do |row|
       if row.even?
         print_empty_entries
+      else
+        print_divide_lines
+      end
+    end
+  end
+
+  def print_new_board
+    board_row_range = (0..4)
+    board_column_range = (0..2)
+    board_row_range.each do |row|
+      if row.even?
+        board_column_range.each do |column|
+          print_entries(entries, column)
+        end
       else
         print_divide_lines
       end
@@ -46,3 +79,5 @@ board.add_entry(Entry.new('X', 3))
 puts(board.entries)
 board.add_entry(Entry.new('O', 4))
 puts(board.entries)
+
+board.print_new_board
